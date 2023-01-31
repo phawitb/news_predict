20c027f04f80ec0c10b797dd79356f74b99bbc09
phawitb <96680592+phawitb@users.noreply.github.com>
2023-01-31 13:21:31 +0700

    Update configure.py

--- configure.py
+++ configure.py
@@ -4,8 +4,10 @@ chrome_headless = True
 
 userdb = "agentai"
 passworddb = "Xx@ukiSDw32dddb"
-hostdb="128.199.73.147"
-dbname="proftech"
+hostdb = "128.199.73.147"
+dbname = "proftech"
+
+path = "/home/agentai/phawit/"
 
 # userdb = "root"
 # passworddb = ""
