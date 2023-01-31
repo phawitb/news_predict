bd324fd1d004eb8e2c9be7f5c2612114a55de683
phawitb <96680592+phawitb@users.noreply.github.com>
2023-01-31 14:36:25 +0700

    Update configure.py

--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ passworddb = "Xx@ukiSDw32dddb"
 hostdb = "128.199.73.147"
 dbname = "proftech"
 
-path = "/home/agentai/phawit/"
+path = "/home/agentai/phawit/Projects/news_predict/"
 batch_size = 100
 
 # userdb = "root"
